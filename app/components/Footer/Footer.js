28b4b65991d81bb21e77ab0d5f7f706c7764a877
Dibyadarshi Das <ddas270@gmail.com>
2021-03-13 22:55:11 +0530

    Update Footer.js

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 
-const Footer = () => (
+const Footer = (props) => (
   <footer className="credits">
-    Made with <span className="heart"></span>by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a>
+    Made with <span className="heart"></span>by <a href={props.link} className="link" target="_blank">{props.copyright}</a>
   </footer>
 );
 
